67f239cdd06ea84b81f1fd81462a0e634534f905
topcue <topcue1019@gmail.com>
2023-09-19 15:16:18 +0900

    Fix wrong path

--- tools/build.sh
+++ tools/build.sh
@@ -9,11 +9,16 @@ git clone git://gcc.gnu.org/git/gcc.git
 cd gcc && git checkout releases/gcc-11.3.0
 
 ## configure and make all
+mkdir -p build install; cd build
 ../configure --prefix="/home/${USER}/llm-opti/tools/install"
-make -j 8 && make install
+make -j 24 && make install
 
-## patch and remake
-cd llm-opti/tools/gcc
-git apply llm-opti/tools/patches/my_changes.patch
+## patch
+cd ${HOME}/llm-opti/tools/gcc
+git apply /home/topcue/llm-opti/tools/patches/quick_sort_flags.patch
+
+## rebuild
+cd ${HOME}/llm-opti/tools/gcc/build
+make -j 24 && make install
 
 # EOF
