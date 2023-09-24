2325b38bdfd3e10604f95729d0a42a67eded5c84
topcue <topcue1019@gmail.com>
2023-09-25 01:10:42 +0900

    Add quicksort with symbols

--- test/quicksort/quicksort_with_symbols.c
+++ test/quicksort/quicksort_with_symbols.c
@@ -4,30 +4,30 @@
 
 #include <stdio.h>
 
-void func1(int *a, int *b) {
+void swap(int *a, int *b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-int func2(int array[], int low, int high) {
+int partition(int array[], int low, int high) {
     int pivot = array[high];
     int i = (low - 1);
     for (int j = low; j < high; j++) {
         if (array[j] <= pivot) {
             i++;
-            func1(&array[i], &array[j]);
+            swap(&array[i], &array[j]);
         }
     }
-    func1(&array[i + 1], &array[high]);
+    swap(&array[i + 1], &array[high]);
     return (i + 1);
 }
 
-void func3(int array[], int low, int high) {
+void quicksort(int array[], int low, int high) {
     if (low < high) {
-        int pi = func2(array, low, high);
-        func3(array, low, pi - 1);
-        func3(array, pi + 1, high);
+        int pi = partition(array, low, high);
+        quicksort(array, low, pi - 1);
+        quicksort(array, pi + 1, high);
     }
 }
 
