11ff83888ce542142b28872c48640954f06b57c2
topcue <topcue1019@gmail.com>
2023-09-25 01:29:40 +0900

    Modify run.sh

--- test/quicksort/run.sh
+++ test/quicksort/run.sh
@@ -4,10 +4,12 @@ GCC_PATH="${HOME}/llm-opti/tools/install/bin/gcc"
 AR_PATH="${HOME}/llm-opti/tools/install/bin/gcc-ar"
 
 # ${GCC_PATH} -S quicksort.c -masm=intel -o tmp.s -O0
+${GCC_PATH} -S quicksort_with_symbols.c -masm=intel -o tmp.s -O0
 
 ${GCC_PATH} -c tmp.s -o tmp.o
 ${AR_PATH} rcs libtmp.a tmp.o
-${GCC_PATH} quicksort_driver.c -O0 -o quicksort_driver_tmp.elf -L. -ltmp
+# ${GCC_PATH} quicksort_driver.c -O0 -o quicksort_driver_tmp.elf -L. -ltmp
+${GCC_PATH} quicksort_driver_with_symbols.c -O0 -o quicksort_driver_tmp.elf -L. -ltmp
 
 time ./quicksort_driver_tmp.elf test
 time ./quicksort_driver_tmp.elf bench
